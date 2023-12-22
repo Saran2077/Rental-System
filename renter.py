dd429e7a96a8b1be160da64f7ba5796cf0ca732e
Saran2077 <saranjamespond123@gmail.com>
2023-12-22 16:06:34 +0530

    Improved Prettytable

--- renter.py
+++ renter.py
@@ -9,9 +9,10 @@ class Renter:
         pass
 
     def can_rent(self, cart, new_item):
-        if len([*cart, new_item]) <= 2:
-            if cart == [] or cart[-1] != new_item[-1]:
-                return True
+        if conn.fetchData(table_name="Garage", columns="AvailabilityStatus", condition=f'WHERE V_ID = {new_item}')[0][0] == 'Available':
+            if len([*cart, new_item]) <= 2:
+                if cart == [] or conn.fetchData(table_name="Garage", columns="Type", condition=f'WHERE V_ID = {cart[0]}') != conn.fetchData(table_name="Garage", columns="Type", condition=f'WHERE V_ID = {new_item}'):
+                    return True
         return False
 
     def cart(self):
@@ -30,6 +31,7 @@ class Renter:
                     self.sno = len(conn.fetchData(table_name="Rental_History", columns="*"))
                     query = f'{self.sno + 1}, {user_id}, {i[0]}, CURRENT_DATE(), "Pending"'
                     conn.row_add(table_name="Rental_History", columns=self.column, values=query)
+                    conn.update(table_name="Garage", column_name="AvailabilityStatus", set_value='"Rented"', condition=f"V_ID = {i[0]}")
                 break
             if self.can_rent(self.cart, a):
                 self.cart.append(a)
